2928bc296e36282b86a47ea93728cf5d364579fd
azeddineya <azeddineya@exemple.com>
2020-05-07 19:07:11 -0400

    added two local calculators

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "sbt-getting-started"
 version := "0.1"
 
 scalaVersion := "2.13.2"
+lazy val root = project.in(file("."))
+  .aggregate(calculators)
+lazy val calculators = project
+scalacOptions := Seq("-unchecked", "-deprecation")
---
+++ calculators/src/main/scala/NetWorth.scala
@@ -0,0 +1,18 @@
+
+
+object NetWorth  extends  App{
+  def calculate(assets: Long, liabilities:Long):Long = assets - liabilities
+
+  override def main(args: Array[String]): Unit = {
+    try{
+      val assets= args(0).toLong
+      val liabilities = args(1).toLong
+      println(s"Your net worth is ${calculate(assets, liabilities)}")
+    }catch {
+      case ex:NumberFormatException => println(s"Please provide a number")
+
+    }
+
+  }
+
+}
